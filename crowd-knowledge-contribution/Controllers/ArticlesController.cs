f1358f6b4d715af085a3d6ba4182169a726dcee2
pitabogdan2002 <bogdan-ioan.pita@s.unibuc.ro>
2023-01-02 18:26:08 +0200

    motor de caute + sortare

--- crowd-knowledge-contribution/Controllers/ArticlesController.cs
+++ crowd-knowledge-contribution/Controllers/ArticlesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 
@@ -27,9 +28,123 @@ namespace crowd_knowledge_contribution.Controllers
             _roleManager = roleManager;
         }
 
-        public IActionResult Index()
+        [Authorize(Roles = "User,Editor,Admin")]
+        public IActionResult Index(string Criteriul)
         {
             var articles = db.Articles.Include("Category").Include("User");
+            switch (Criteriul)
+            {
+                case "Alfabetic":
+                    articles = articles.OrderBy(s => s.Title);
+                    break;
+                case "Alfabetic_Inv":
+                    articles = articles.OrderByDescending(s => s.Title);
+                    break;
+                case "Data":
+                    articles = articles.OrderBy(s => s.Date);
+                    break;
+                case "Data_Inv":
+                    articles = articles.OrderByDescending(s => s.Date);
+                    break;
+                default:
+                    articles = articles.OrderBy(s => s.Date);
+                    break;
+            }
+
+            var search = "";
+
+            // MOTOR DE CAUTARE
+
+            if (Convert.ToString(HttpContext.Request.Query["search"]) != null)
+            {
+                search = Convert.ToString(HttpContext.Request.Query["search"]).Trim(); // eliminam spatiile libere 
+
+                // Cautare in articol (Title si Content)
+
+                List<int> articleIds = db.Articles.Where
+                                        (
+                                         at => at.Title.Contains(search)
+                                         || at.Content.Contains(search)
+                                        ).Select(a => a.Id).ToList();
+
+                // Cautare in comentarii (Content)
+                List<int> articleIdsOfCommentsWithSearchString = db.Comments
+                                        .Where
+                                        (
+                                         c => c.Content.Contains(search)
+                                        ).Select(c => (int)c.ArticleId).ToList();
+
+                // Se formeaza o singura lista formata din toate id-urile selectate anterior
+                List<int> mergedIds = articleIds.Union(articleIdsOfCommentsWithSearchString).ToList();
+
+
+                // Lista articolelor care contin cuvantul cautat
+                // fie in articol -> Title si Content
+                // fie in comentarii -> Content
+                articles = db.Articles.Where(article => mergedIds.Contains(article.Id))
+                                      .Include("Category")
+                                      .Include("User")
+                                      .OrderBy(a => a.Date);
+
+            }
+
+            ViewBag.SearchString = search;
+
+            // AFISARE PAGINATA
+
+            // Alegem sa afisam 3 articole pe pagina
+            int _perPage = 3;
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+
+
+            // Fiind un numar variabil de articole, verificam de fiecare data utilizand 
+            // metoda Count()
+
+            int totalItems = articles.Count();
+
+
+            // Se preia pagina curenta din View-ul asociat
+            // Numarul paginii este valoarea parametrului page din ruta
+            // /Articles/Index?page=valoare
+
+            var currentPage = Convert.ToInt32(HttpContext.Request.Query["page"]);
+
+            // Pentru prima pagina offsetul o sa fie zero
+            // Pentru pagina 2 o sa fie 3 
+            // Asadar offsetul este egal cu numarul de articole care au fost deja afisate pe paginile anterioare
+            var offset = 0;
+
+            // Se calculeaza offsetul in functie de numarul paginii la care suntem
+            if (!currentPage.Equals(0))
+            {
+                offset = (currentPage - 1) * _perPage;
+            }
+
+            // Se preiau articolele corespunzatoare pentru fiecare pagina la care ne aflam 
+            // in functie de offset
+            var paginatedArticles = articles.Skip(offset).Take(_perPage);
+
+
+            // Preluam numarul ultimei pagini
+
+            ViewBag.lastPage = Math.Ceiling((float)totalItems / (float)_perPage);
+
+            // Trimitem articolele cu ajutorul unui ViewBag catre View-ul corespunzator
+            ViewBag.Articles = paginatedArticles;
+
+            if (search != "")
+            {
+                ViewBag.PaginationBaseUrl = "/Articles/Index/?search=" + search + "&page";
+            }
+            else
+            {
+                ViewBag.PaginationBaseUrl = "/Articles/Index/?page";
+            }
+
 
             Article article = new Article();
 
